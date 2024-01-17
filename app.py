b771c37208d06b01ef4446aa5038b2ae7f064507
Arnaud RAFALIMANANA--CHAN PENG <arnaud.rafalimanana--chan.peng@efrei.net>
2024-01-17 16:05:33 +0100

    change format url request

--- app.py
+++ app.py
@@ -3,16 +3,29 @@ import joblib
 
 app = Flask(__name__)
 
-# Charger le modèle
 model = joblib.load('iris_knn_model.joblib')
 
-@app.route('/predict', methods=['POST'])
+@app.route('/predict', methods=['GET', 'POST'])
 def predict():
-    data = request.get_json()
-    features = data.get('features')
+    if request.method == 'GET':
+        # Traitement des paramètres de la chaîne de requête pour une requête GET
+        sepal_length = float(request.args.get('sepal_length'))
+        sepal_width = float(request.args.get('sepal_width'))
+        petal_length = float(request.args.get('petal_length'))
+        petal_width = float(request.args.get('petal_width'))
+    elif request.method == 'POST':
+        # Traitement du corps JSON pour une requête POST
+        data = request.get_json(force=True)
+        sepal_length = float(data['sepal_length'])
+        sepal_width = float(data['sepal_width'])
+        petal_length = float(data['petal_length'])
+        petal_width = float(data['petal_width'])
+    else:
+        return jsonify({"error": "Method not allowed"}), 405
+
+    features = [sepal_length, sepal_width, petal_length, petal_width]
     prediction = model.predict([features])
-    return jsonify({'prediction': int(prediction[0])})
+    return jsonify(prediction.tolist())
 
 if __name__ == '__main__':
-    # app.run(debug=True)
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0',port=5000)
