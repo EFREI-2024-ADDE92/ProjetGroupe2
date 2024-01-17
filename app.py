6f25a2def9e36e37cdca78aec9d2acde816d2028
STANG94 <stephane.tang@efrei.net>
2024-01-17 15:11:07 +0100

    update

--- app.py
+++ app.py
@@ -3,33 +3,6 @@ import joblib
 
 app = Flask(__name__)
 
-<<<<<<< HEAD
-model = joblib.load('knn_model.joblib')
-
-@app.route('/', methods=['GET', 'POST'])
-def predict():
-    if request.method == 'GET':
-        # Traitement des paramètres de la chaîne de requête pour une requête GET
-        sepal_length = float(request.args.get('sepal_length'))
-        sepal_width = float(request.args.get('sepal_width'))
-        petal_length = float(request.args.get('petal_length'))
-        petal_width = float(request.args.get('petal_width'))
-    elif request.method == 'POST':
-        # Traitement du corps JSON pour une requête POST
-        data = request.get_json(force=True)
-        sepal_length = float(data['sepal_length'])
-        sepal_width = float(data['sepal_width'])
-        petal_length = float(data['petal_length'])
-        petal_width = float(data['petal_width'])
-    else:
-        return jsonify({"error": "Method not allowed"}), 405
-
-    features = [sepal_length, sepal_width, petal_length, petal_width]
-    prediction = model.predict([features])
-    return jsonify(prediction.tolist())
-
-if __name__ == '__main__':
-=======
 # Charger le modèle
 model = joblib.load('iris_knn_model.joblib')
 
@@ -42,5 +15,4 @@ def predict():
 
 if __name__ == '__main__':
     # app.run(debug=True)
->>>>>>> 62f2a3d9e2cc7fbddaecc901c2dbef5399d7d99f
     app.run(host='0.0.0.0', port=5000)
--- dockerfile
+++ dockerfile
@@ -1,15 +1,22 @@
-FROM python:3.11.1-slim-buster
+FROM ubuntu:latest
 
-ENV VIRTUAL_ENV=/venv
-RUN python3 -m venv $VIRTUAL_ENV
-ENV PATH="$VIRTUAL_ENV/bin:$PATH"
+# FROM python:3.11-alpine3.17
 
+RUN apt-get update && \
+    DEBIAN_FRONTEND=noninteractive apt-get upgrade -yq && \
+    apt-get install -y python3 python3-pip && \
+    rm -rf /var/lib/apt/lists/*
+
+WORKDIR /app
 
 COPY requirements.txt .
-RUN pip install --no-cache-dir --requirement requirements.txt
 
-EXPOSE 5000
+RUN pip3 install --no-cache-dir -r requirements.txt
+
+# RUN apk add libcrypto3=3.1.1-r1 libssl3=3.1.1-r1 --repository=https://dl-cdn.alpinelinux.org/alpine/edge/main --no-cache
 
 COPY . .
 
-CMD [ "python", "app.py" ]
+EXPOSE 5000
+
+CMD ["python3", "app.py"]
