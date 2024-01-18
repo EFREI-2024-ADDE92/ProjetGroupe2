f85a07ba5167fd42ee98f7d881fa567f96752d36
dzungpnguyen <thienhoangphuongdung@gmail.com>
2024-01-18 15:45:49 +0100

    update azure url

--- testcharge.js
+++ testcharge.js
@@ -8,7 +8,7 @@ export let options = {
 
 export default function () {
   // Définissez l'URL de votre API
-  const url = 'https://adde92-gr2-container.blackcoast-633d2335.francecentral.azurecontainerapps.io/predict';
+  const url = 'https://adde92-gr2-container.happycliff-35c01287.francecentral.azurecontainerapps.io/predict';
 
   // Définissez les données JSON pour la requête POST
   const payload = {"sepal_length": 1.2, "sepal_width": 1.2, "petal_length": 1.2, "petal_width": 1.2}
